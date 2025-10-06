edc7582d3d6ac380d06fb0aefd3e0de6ff35c9c3
f-seiko <s2112137@st.tohtech.ac.jp>
2025-10-06 16:25:17 +0900

    server.jsの修正

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // server.js
 const { app } = require("./app");
-
 const PORT = process.env.PORT || 3000;
-console.log(`[server] PORT=${PORT} NODE_ENV=${process.env.NODE_ENV}`);
+app.listen(PORT, () => console.log(`[server] listening on ${PORT}`));
